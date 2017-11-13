acb90650cca1c45e468cb232079d95c7bd8d4d5b
Banashankar Veerad <banveerad@users.noreply.github.com>
2017-11-12 22:47:28 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from flask import got_request_exception
 import os
 import rollbar
+import rollbar.contrib.flask
 import webob
 
-rollbar.init('')
 app = Flask(__name__)
+
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token for the demo app: https://rollbar.com/demo
+        '',
+        # environment name
+        'flasktest_bana',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route('/')
 def hello_world():
@@ -23,7 +40,6 @@ def get_details(name):
         response.append(data)
         return webob.Response(json=response, content_type='application/json')
     except Exception as e:
-        rollbar.report_message('File name: %s %s' % (name, e.strerror))
         return webob.Response(
             json=dict(message="Invalid request", details=e.strerror),
             status=400)
